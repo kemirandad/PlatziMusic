6c5434093cdf7e154d3247d4dbf232eaa01ab47b
kemiranda <kjmd2s@gmail.com>
2020-12-25 09:12:37 -0500

    Primer commit

---
+++ app/src/main/java/com/example/platzimusic/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.platzimusic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //val test1 = findViewById<TextView?>(R.id.tvMainTitle)
+        //val b: Int = if (a != null) a.length else -1
+        //val b = a?.length ?: -1
+        //val condicion = test1?.length() ?: -1
+
+    }
+}
